6dc2f12a38079bdbd3e2c59797e6ff27eb6a7369
Tristan Gray <tristangraycc@gmail.com>
2024-02-18 23:30:02 +0000

    Fixing robots txt

--- src/Pages/Blog/BlogPage.tsx
+++ src/Pages/Blog/BlogPage.tsx
@@ -8,7 +8,7 @@ function BlogPage() {
         <div className="blog-page">
             <div className="half">
                 <img className="arrow arrow-one" src={ arrow } alt="Arrow"/>
-                <h1><a href="https://ko-fi.com/tristangray">Link to Blog</a></h1>
+                <h1><a href="https://ko-fi.com/tristangray/posts">Link to Blog</a></h1>
                 <img className="arrow arrow-two" src={ arrow } alt="Arrow"/>
             </div>
         </div>
